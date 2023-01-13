1260703e9632dcd7f62787e8666ae6e128ebe6e2
Ikramul Haque Oli <ikramul.haque@agoda.com>
2023-01-14 02:37:09 +0700

    Add starter project files

---
+++ PlanBee.University-portal.backend/Controllers/HealthCheckController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlanBee.University_portal.backend.Controllers;
+
+[ApiController]
+//[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+public class HealthCheckController : ControllerBase
+{
+    private readonly ILogger<HealthCheckController> _logger;
+
+    public HealthCheckController(ILogger<HealthCheckController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(template: "healthcheck")]
+    public ActionResult<string> Get()
+    {
+        return Ok("Healthy");
+    }
+    
+    [HttpGet(template: "healthcheck/authorize")]
+    [Authorize]
+    public ActionResult<string> Authorize()
+    {
+        return Ok("Authorized");
+    }
+}
