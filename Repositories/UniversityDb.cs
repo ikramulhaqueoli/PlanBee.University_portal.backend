8494158bae371a5b35efe22dcad29b4cdae2c936
Ikramul Haque Oli <ikramul.haque@agoda.com>
2023-01-15 01:26:55 +0700

    Delete unnecessary file

--- Repositories/UniversityDb.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-using PlanBee.University_portal.backend.Domain.Entities;
-
-namespace PlanBee.University_portal.backend.Repositories;
-
-public partial class UniversityDb
-{
-    public DbSet<BaseUser> BaseUsers { get; set; } = null!;
-}
