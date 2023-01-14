13e5964f5029e7b5f11c9efb24ba1f6d36772cf5
Ikramul Haque Oli <ikramul.haque@agoda.com>
2023-01-15 01:28:21 +0700

    Add Initial Migration

---
+++ Start/Migrations/20230114182748_InitialCreate-15-01-2023.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlanBee.Universityportal.backend.Start.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate15012023 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
